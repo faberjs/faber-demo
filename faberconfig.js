97485fa745525640464ee3f7070b9e1d690188bf
André Ferraz <15928408+andreferraz@users.noreply.github.com>
2025-01-14 21:20:58 -0300

    feat: initial commit with docs and faberconfig

---
+++ faberconfig.js
@@ -0,0 +1,19 @@
+module.exports = function (faber) {
+	faber.setActions((data) => {
+		return [
+			// Replace placeholders in README.md
+			{
+				type: 'replace',
+				files: '_README.md',
+				from: ['PLACEHOLDER_NAME', 'PLACEHOLDER_AUTHOR'],
+				to: [data.name, data.author],
+			},
+			// Rename _README.md to README.md (overriding the default README.md)
+			{
+				type: 'move',
+				from: '_README.md',
+				to: 'README.md',
+			}
+		];
+	});
+};
